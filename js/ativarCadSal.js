7d79bb409fb5574c32687c10d8c4f2078026f39f
Filipe Oliveira <filipe.guice@gmail.com>
2018-11-15 12:28:57 -0200

    Adicionando a validação de cpf pro formulario do salao(Não testado pode estar dando erro mas vemos isso depois)

--- js/ativarCadSal.js
+++ js/ativarCadSal.js
@@ -3,34 +3,52 @@ $('.formularioSalao').submit(function (e) {
     //Usando ajax para chamar a pagina php especifica e passa o method que as informações estão indo
     var form = $(this);
     var url = form.attr('action');
-    $.ajax({
-        url: url,
-        type: 'POST',
-        data: $('.formularioSalao').serialize(),
-        // função caso tudo ocorra normalmente
-        success: function (data) {
-            if (data != "Salão cadastrado com sucesso") {
-                swal({
-                    type: 'error',
-                    title: 'Oops...',
-                    text: data,
-                })
-                
-            }
-            else {
-                
-                swal({
-                    type: 'success',
-                    title: 'OK',
-                    text: data,
-                    confirmButtonColor: '#600ab1',
-                    confirmButtonText: 'OK',
-                })
-                    
+    $('#cpf1').cpfcnpj({
+        mask: true,
+        validate: 'cpfcnpj',
+        event: 'click',
+        //validateOnlyFocus: true,
+        handler: '.submit',
+        ifValid: function (input) {
+            //Usando ajax para chamar a pagina php especifica e passa o method que as informações estão indo
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: $('.formularioSalao').serialize(),
+                //função caso tudo ocorra normalmente
+                success: function (data) {
+                    if (data != "Salão cadastrado com sucesso") {
+                        swal({
+                            type: 'error',
+                            title: 'Oops...',
+                            text: data,
+                        })
+
+                    }
+                    else {
+
+                        swal({
+                            type: 'success',
+                            title: 'OK',
+                            text: data,
+                            confirmButtonColor: '#600ab1',
+                            confirmButtonText: 'OK',
+                        })
+
                         window.location.replace("Home.shtml");
-                
-            }
-            
+
+                    }
+
+                }
+            });
+
+        },
+        ifInvalid: function (input) {
+            swal({
+                type: 'error',
+                title: 'Oopss',
+                text: "CPF Inválido",
+            })
         }
     });
     // return false para a pagina não ser atualizada
